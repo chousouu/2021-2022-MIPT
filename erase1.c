80e39fdd4e3ee7afe750ffcc9f1d71a668c40785
Ясин Мамедов <mamedov.iakh@phystech.edu>
2022-04-15 02:02:52 +0300

    need to fix when we transplant root element

--- erase1.c
+++ erase1.c
@@ -4,25 +4,38 @@ struct Node {
     int val;
     struct Node *left;
     struct Node *right;
+    struct Node *parent;
 };
 
 struct Node * Insert(struct Node * tree, int x);
+struct Node *Minimum(struct Node *tree); 
+struct Node *Search(struct Node *tree, int k);
+struct Node *parent_search(struct Node *tree, struct Node *Search); 
+void Transplant(struct Node *tree, struct Node *current, struct Node *changeTo);
+void Erase(struct Node *tree, struct Node *Find);
 void InOrder(struct Node *tree);
-
-
-
 void tree_destroy(struct Node * tree);
 
 int main() {
     struct Node * tree = NULL;
     int x = 0;
+    printf("fill a binary tree\nType 0 to stop : \n");
     scanf("%d", &x);
     while (x != 0) {
         tree = Insert(tree, x);
         scanf("%d", &x);
     }
     InOrder(tree);
+    printf("\n");
+    printf("Type element you want to erase:");
+    int k = 0;
+    scanf("%d", &k); // элемент который удаляем
+    printf("\n");
+    struct Node *Find = Search(tree, k);
+    Erase(tree, Find);
+    InOrder(tree);
     tree_destroy(tree);
+    
     return 0;
 }
 
@@ -47,6 +60,81 @@ struct Node * Insert(struct Node * tree, int x) {
     return tree;
 }
 
+struct Node *Minimum(struct Node *tree) {
+    if (tree->left == NULL) {
+        return tree;
+    }
+    return Minimum(tree->left);
+}
+
+struct Node* Search(struct Node *tree, int k) {
+    if ((tree == NULL) || (k == tree->val)) {
+        return tree;
+    } 
+    if (k < tree->val) {
+        return Search(tree->left, k);
+    }
+    else {
+        return Search(tree->right, k);
+    }
+}
+struct Node *parent_search(struct Node *tree, struct Node *Search) {
+    if ((tree->right == Search) || ((tree->left) == Search)) {
+        return tree;
+    }
+    else {
+        if (tree->val < Search->val) {
+            parent_search(tree->right, Search);
+        }
+        else {
+            parent_search(tree->left, Search);
+        }
+    }
+}
+
+
+
+
+void Transplant(struct Node *tree, struct Node *current, struct Node *changeTo) {
+    struct Node *current_parent = parent_search(tree, current);
+    if ( current = current_parent->left) {
+        current_parent->left = changeTo;
+    }
+    else {
+        current_parent->right = changeTo;
+    }
+    if (changeTo != NULL) {
+        struct Node *changeto_parent = parent_search(tree, changeTo);
+        changeto_parent = current_parent;
+    }
+}
+
+void Erase(struct Node *tree, struct Node *z) {  // z - хотим удалить
+    if (z->left == NULL) {
+        Transplant(tree, z, z->right);
+    }
+    else if (z->right == NULL) {
+        Transplant(tree, z, z->left);
+    }
+    else {
+        struct Node *y = Minimum(z->right);
+        struct Node *y_parent = parent_search(tree, y);
+        struct Node *y_left = parent_search(tree, y->left);
+        struct Node *y_right = parent_search(tree, y->right);
+        if (y_parent != z) {
+            Transplant(tree,y, y->right);
+            y->right = z->right;
+            y_right = y; 
+        }
+        Transplant(tree, z, y);
+        y->left = z->left;
+        y_left = y;
+    }
+
+}
+
+
+
 void InOrder(struct Node *tree) {
     if (tree != NULL) {
         InOrder(tree->left);
@@ -54,11 +142,6 @@ void InOrder(struct Node *tree) {
         InOrder(tree->right);
     }
 }
-
-
-
-
-
 
 void tree_destroy(struct Node * tree) {
     if(tree != NULL) {
