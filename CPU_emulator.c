3617a6a6fccc75697d651dcaa8d5fa25aeb60907
Ясин Мамедов <mamedov.iakh@phystech.edu>
2022-04-24 23:32:30 +0300

    переделать,чтобы не надо было жать дважды энтер, ибо из-за этого перестать цикл while работать на 3 раз.

--- CPU_emulator.c
+++ CPU_emulator.c
@@ -11,6 +11,8 @@ struct Info {
 int detect_symbol_one(char *command);
 int detect_symbol_two(char *command);
 void mov(char* second_register, struct Info *p_Registers, int k);
+void add(char* second_register, struct Info *p_Registers, int k);
+void sub(char* second_register, struct Info *p_Registers, int k);
 void reg_printer(struct Info *p_Registers);
 
 
@@ -47,14 +49,35 @@ int main() {
                 mov(second_register, p_Registers, k);
                 reg_printer(p_Registers);
                 break;
+            case 'a':
+                add(second_register, p_Registers, k);
+                reg_printer(p_Registers);
+                break;
             case 's':
-
+                sub(second_register, p_Registers, k);
+                reg_printer(p_Registers);
                 break;
          }
     }
 }
 
 
+
+
+int detect_symbol_one(char *command) { //detects first registers 2nd symbol(%) -> ex: mov e%x ...
+    if (command[5] == 'a') {
+        return 0;
+    }
+    if (command[5] == 'b') {
+        return 1;
+    }
+    if (command[5] == 'c') {
+        return 2;
+    }
+    if (command[5] == 'd') {
+        return 3;
+    }  
+}
 
 int detect_symbol_two(char *second_register) { //detects 2nd registers 2nd symbol(%) -> ex: mov ebx e%x;
     if (second_register[1] == 'a') {
@@ -71,19 +94,27 @@ int detect_symbol_two(char *second_register) { //detects 2nd registers 2nd symbo
     }  
 }
 
-int detect_symbol_one(char *command) { //detects first registers 2nd symbol(%) -> ex: mov e%x ...
-    if (command[5] == 'a') {
-        return 0;
+int value_of_second_reg(struct Info *p_Registers, char* second_register) {
+    int number = detect_symbol_two(second_register);
+    printf("number : %d\n", number);
+    int first_reg_value;
+    if (number == 0) {
+        first_reg_value = (int)p_Registers->eax;
+        printf(" first_reg_value : %d\n", first_reg_value);
     }
-    if (command[5] == 'b') {
-        return 1;
+    if (number == 1) {
+        first_reg_value = (int)p_Registers->ebx;
+        printf(" first_reg_value : %d\n", first_reg_value);
     }
-    if (command[5] == 'c') {
-        return 2;
+    if (number == 2) {
+        first_reg_value = (int)p_Registers->ecx;
+        printf(" first_reg_value : %d\n", first_reg_value);
     }
-    if (command[5] == 'd') {
-        return 3;
-    }  
+    if (number == 3) {
+        first_reg_value = (int)p_Registers->edx;
+        printf(" first_reg_value : %d\n", first_reg_value);
+    }
+    return first_reg_value;
 }
 
 void mov(char* second_register, struct Info *p_Registers, int k) {  //k - 2nd symbol 1st reg
@@ -109,26 +140,7 @@ void mov(char* second_register, struct Info *p_Registers, int k) {  //k - 2nd sy
         }
     }
     if (second_register[0] == 'e') {
-        int number = detect_symbol_two(second_register);
-        printf("number : %d\n", number);
-        int first_reg_value;
-
-        if (number == 0) {
-            first_reg_value = (int)p_Registers->eax;
-            printf(" first_reg_value : %d\n", first_reg_value);
-        }
-        if (number == 1) {
-            first_reg_value = (int)p_Registers->ebx;
-            printf(" first_reg_value : %d\n", first_reg_value);
-        }
-        if (number == 2) {
-            first_reg_value = (int)p_Registers->ecx;
-            printf(" first_reg_value : %d\n", first_reg_value);
-        }
-        if (number == 3) {
-            first_reg_value = (int)p_Registers->edx;
-            printf(" first_reg_value : %d\n", first_reg_value);
-        }
+        int first_reg_value = value_of_second_reg(p_Registers, second_register);
 
         switch(k) {
             case 0: 
@@ -152,6 +164,99 @@ void mov(char* second_register, struct Info *p_Registers, int k) {  //k - 2nd sy
     return;
 }
 
+void add(char* second_register, struct Info *p_Registers, int k) {
+    printf("second reg : %c\n", second_register[0]);
+    printf("k : %d\n", k);
+    reg_printer(p_Registers);
+    if (second_register[0] != 'e') {
+        int int_char = second_register[0] - 48; //число, явл. вторым регистром
+        printf("int_char : %d\n", int_char);
+        switch(k) {
+            case 0: 
+                p_Registers->eax += int_char;
+                break;
+            case 1: 
+                p_Registers->ebx += int_char;
+                break;
+            case 2: 
+                p_Registers->ecx += int_char;
+                break;
+            case 3: 
+                p_Registers->edx += int_char;  
+                break;  
+        }
+    }
+    if (second_register[0] == 'e') {
+        int first_reg_value = value_of_second_reg(p_Registers, second_register);
+
+        switch(k) {
+            case 0: 
+                p_Registers->eax += first_reg_value;
+                reg_printer(p_Registers);
+                break;
+            case 1: 
+                p_Registers->ebx += first_reg_value;
+                reg_printer(p_Registers);
+                break;
+            case 2: 
+                p_Registers->ecx += first_reg_value;
+                reg_printer(p_Registers);
+                break;
+            case 3: 
+                p_Registers->edx += first_reg_value;  
+                reg_printer(p_Registers);
+                break;  
+        }
+    }
+    return;
+}
+
+void sub(char* second_register, struct Info *p_Registers, int k) {
+    printf("second reg : %c\n", second_register[0]);
+    printf("k : %d\n", k);
+    reg_printer(p_Registers);
+    if (second_register[0] != 'e') {
+        int int_char = second_register[0] - 48; //число, явл. вторым регистром
+        printf("int_char : %d\n", int_char);
+        switch(k) {
+            case 0: 
+                p_Registers->eax -= int_char;
+                break;
+            case 1: 
+                p_Registers->ebx -= int_char;
+                break;
+            case 2: 
+                p_Registers->ecx -= int_char;
+                break;
+            case 3: 
+                p_Registers->edx -= int_char;  
+                break;  
+        }
+    }
+    if (second_register[0] == 'e') {
+        int first_reg_value = value_of_second_reg(p_Registers, second_register);
+
+        switch(k) {
+            case 0: 
+                p_Registers->eax -= first_reg_value;
+                reg_printer(p_Registers);
+                break;
+            case 1: 
+                p_Registers->ebx -= first_reg_value;
+                reg_printer(p_Registers);
+                break;
+            case 2: 
+                p_Registers->ecx -= first_reg_value;
+                reg_printer(p_Registers);
+                break;
+            case 3: 
+                p_Registers->edx -= first_reg_value;  
+                reg_printer(p_Registers);
+                break;  
+        }
+    }
+    return;
+}
 
 
 void reg_printer(struct Info *p_Registers) {
