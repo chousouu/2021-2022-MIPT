88fa3fa2e7f9de22f2f5bb69ed86233223bf99d7
Ясин Мамедов <mamedov.iakh@phystech.edu>
2022-05-09 01:51:31 +0300

    now works with symbols too (kinda)

--- Inline_ASM.c
+++ Inline_ASM.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#define N 4
 int main()
 {
     //A 65 Z90; a 97 - z122;
+    // 48 - 57  : 0 - 9 
     char arr[100];
     char a;
     int i = 0;
@@ -11,9 +11,17 @@ int main()
         i++;
         a = getchar();
     }
+    char array_of_non_letters[100];
+    int arr_index[100] = { 0 } ;// массив, хранящий номер элемента arr, где не буква
+    for (int i = 0; i < 100; i++) {
+        if ( ((int)arr[i] <= 97 && (int)arr[i] >= 90) || (int)arr[i] > 122 || (int)arr[i] < 65  ) {
+            array_of_non_letters[i] = arr[i];
+            arr_index[i] = i;
+        }
+    }
     long long N_count = i;
     long long res = 0;
-    long long diff = 32;
+    long long A_razn_a = 32;
     char * p_arr = arr;
 
 asm volatile (
@@ -42,15 +50,19 @@ asm volatile (
 
 		".att_syntax noprefix"
 		: "=d" (res)
-		: "a" (p_arr), "c" (i), "d" (diff)
+		: "a" (p_arr), "c" (i), "d" (A_razn_a)
 	);
 	
-	printf("\narr = %p, diff = %llu\n\n", p_arr, res);
-	
+	//printf("\narr = %p, A_razn_a = %llu\n\n", p_arr, res);
+	for (int i = 0; i < 100; i++) {
+        if (arr_index[i] != 0) {
+            arr[i] = array_of_non_letters[i];
+        }
+    }
+
     for(int j = 0; j < i; j++) {
         printf("%c", arr[j]);
     }
 
     return 0;
 }
-
